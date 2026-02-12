5292170a301fb4e8712c790f8746f6828e9e69ba
suramkavyasree <suramkavyasree5@gmail.com>
2026-02-12 19:46:20 +0530

    commit 2

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -4,7 +4,7 @@ import EmailPasswordAuthForm from "@/auth/EmailPasswordAuthForm";
 
 export default function LoginPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-2900">
+    <div className="min-h-screen flex items-center justify-center bg-gray-5900">
       <EmailPasswordAuthForm />
     </div>
   );
