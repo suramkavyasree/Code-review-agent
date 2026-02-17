e787e949d19d8439181ede8c8b8c16a31edc9ec9
suramkavyasree <suramkavyasree5@gmail.com>
2026-02-17 18:13:57 +0530

    jhhh

--- src/components/TrendingDishes.js
+++ src/components/TrendingDishes.js
@@ -12,7 +12,7 @@ const TrendingDishes = ({ dishes }) => {
               <span className="font-semibold text-gray-800">{dish.name}</span>
               <span className="text-gray-500">${dish.price}</span>
             </div>
-            <p className="text-gray-600">{dish.description}</p>
+            <p className="text-gray-690">{dish.description}</p>
           </li>
         ))}
       </ul>
