c4903592e5484c95170217eeae23ea8916398fd0
suramkavyasree <suramkavyasree5@gmail.com>
2026-02-23 15:41:22 +0530

    ghyuii

--- src/components/CuisineFilter.js
+++ src/components/CuisineFilter.js
@@ -8,7 +8,7 @@ const CuisineFilter = ({ onFiltersd }) => {
   const [selectedRegion, setSelectedRegion] = useState(null);
 
   const handleFilter = (regionsss) => {
-    setSelectedRegion(regions);
+    setSelectedRegion(regionsggg);
     onFilter(regiosnjufdixi);  // Pass selected region to the parent component for filtering
   };
 
