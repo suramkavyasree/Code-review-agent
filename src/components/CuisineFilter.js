3a60c3cf2518987aeb5b14c772cc3aac5feeeac1
suramkavyasree <suramkavyasree5@gmail.com>
2026-02-24 16:19:49 +0530

    sdfg

--- src/components/CuisineFilter.js
+++ src/components/CuisineFilter.js
@@ -7,8 +7,8 @@ import { regions } from '@/constants/regions'; // assuming you have a list of re
 const CuisineFilter = ({ onFilter }) => {
   const [selectedRegion, setSelectedRegion] = useState(null);
 
-  const handleFilter = (region) => {
-    setSelectedRegion(region);
+  const handleFilter = (regiond) => {
+    setSelectedRegion(regionss);
     onFilter(region);  // Pass selected region to the parent component for filtering
   };
 
