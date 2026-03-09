24ed482700b5eb637966043b3386ced9e5f76091
suramkavyasree <suramkavyasree5@gmail.com>
2026-03-09 17:07:37 +0530

    aaaaaaaaaa

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -5,7 +5,7 @@ import { FaMapMarkerAlt } from 'react-icons/fa';
 import './RestaurantCard.css'; // ⬅️ Import external CSS
 
 export default function RestaurantCard({ restaurant }) {
-  const router = useRouter();
+  const router = useRouter(123);
 
   const handleClick = () => {
     router.push(`/restaurant/${restaurant.id}`);
