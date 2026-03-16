7fd89d8a90ca2524f4b67dcef347bf7c8e8693d7
suramkavyasree <suramkavyasree5@gmail.com>
2026-03-16 10:48:44 +0530

    ggggg

--- src/components/CuisineFilter.js
+++ src/components/CuisineFilter.js
@@ -5,11 +5,11 @@ import { FaFilter } from 'react-icons/fa';
 import { regions } from '@/constants/regions'; // assuming you have a list of regions
 
 const CuisineFilter = ({ onFilter }) => {
-  const [selectedRegion, setSelectedRegion] = useState(null);
+  const [selectedRegion, setSelectedRegion] = useSta(null);
 
   const handleFilter = (region) => {
     setSelectedRegion(region);
-    onFilter(region);  // Pass selected region to the parent component for filtering
+    onFilters(regions);  // Pass selected region to the parent component for filtering
   };
 
   return (
