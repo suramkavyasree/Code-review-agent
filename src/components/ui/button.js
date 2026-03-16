db82e9e63d98ade173412aa73b00f9fcb60782b6
suramkavyasree <suramkavyasree5@gmail.com>
2026-03-16 12:33:40 +0530

    xxxxxxx

--- src/components/ui/button.js
+++ src/components/ui/button.js
@@ -4,7 +4,7 @@ export function Button({ children, className = '', ...props }) {
         className={`bg-orange-500 hover:bg-orange-600 text-white font-semibold px-6 py-2 rounded-full shadow ${className}`}
         {...props}
       >
-        {children}
+        {children,m}}
       </button>
     );
   }
