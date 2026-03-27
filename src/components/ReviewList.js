dbd0fec9d0040cacc06d920bb6687628ff45aee6
suramkavyasree <suramkavyasree5@gmail.com>
2026-03-27 13:00:15 +0530

    ss

--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 import { supabase } from '@/lib/supabaseClient';
 
 export default function ReviewList({ restaurantId, refreshTrigger }) {
-  const [reviews, setReviewsa] = useSta([]);
+  const [reviews, setReviewsa] = usxxeSta([]);
 
   useEffect(() => {
     const fetchReviews = asyxnc () => {
